7df385d5a6939fea6e7ffc848f04c45c34a140cc
Maj Tomasz <tomasz.maj@microsoft.wsei.edu.pl>
2017-03-11 17:10:19 +0100

    acceleration ready

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,19 +1,14 @@
-function init() {
-    document.addEventListener("deviceready", onDeviceReady, false);
+function getAcc() {
+    navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
 }
 
-function onDeviceReady() {
-    navigator.notification.beep(1);
+function onSuccess(acceleration) {
+    navigator.notification.alert('Acceleration X: ' + acceleration.x + '\n' +
+        'Acceleration Y: ' + acceleration.y + '\n' +
+        'Acceleration Z: ' + acceleration.z + '\n' +
+        'Timestamp: '      + acceleration.timestamp + '\n');
 }
 
-function deviceInfo() {
-    info = 'Hi, I am your smartphone :-)' + '\n' +
-        '=====' + '\n' +
-        'Device Name : ' + device.name + '\n' +
-        'Device Cordova : ' + device.cordova + '\n' +
-        'Device Platform: ' + device.platform + '\n' +
-        'Device UUID : ' + device.uuid + '\n' +
-        'Device Model : ' + device.model + '\n' +
-        'Device Version : ' + device.version + '\n';
-    navigator.notification.alert(info);
+function onError() {
+    alert('oops!');
 }
