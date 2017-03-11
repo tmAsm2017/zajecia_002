14d5e82ee6ebd02ca4434b2543bbfc59e359044e
Maj Tomasz <tomasz.maj@microsoft.wsei.edu.pl>
2017-03-11 17:18:52 +0100

    networking

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,19 +1,28 @@
 function init() {
     document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("offline", onOffline, false);
 }
 
 function onDeviceReady() {
-    navigator.notification.beep(1);
+    checkConnection();
 }
 
-function deviceInfo() {
-    info = 'Hi, I am your smartphone :-)' + '\n' +
-        '=====' + '\n' +
-        'Device Name : ' + device.name + '\n' +
-        'Device Cordova : ' + device.cordova + '\n' +
-        'Device Platform: ' + device.platform + '\n' +
-        'Device UUID : ' + device.uuid + '\n' +
-        'Device Model : ' + device.model + '\n' +
-        'Device Version : ' + device.version + '\n';
-    navigator.notification.alert(info);
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    navigator.notification.alert('Connection type: ' + states[networkState]);
+}
+
+function onOffline() {
+    navigator.notification.alert('Device turned offline');
 }
